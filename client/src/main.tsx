5dcf87d0b4943ef85d26de30acdfea70ab496794
michael705 <37574674-michael705@users.noreply.replit.com>
2024-11-25 14:07:51 +0000

    Agent query: Is the admin dashboard loading correctly now? Can you access the login page and navigate to the admin sections?

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode, useEffect } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Switch, Route, useLocation } from "wouter";
 import "./index.css";
