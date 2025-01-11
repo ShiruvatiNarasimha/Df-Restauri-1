6adf025138c1ee592f105bc5b0036ec661b808e0
Shiruvati <mrnattyboy05@gmail.com>
2025-01-11 22:56:06 +0530

    Update schema.ts

--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,2 @@
 import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
 import type { InferModel } from "drizzle-orm";
-import { createInsertSchema, createSelectSchema } from "drizzle-zod";
-import { z } from "zod";
-
-export const users = pgTable("users", {
-  
