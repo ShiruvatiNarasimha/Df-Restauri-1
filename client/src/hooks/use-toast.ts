39692872f9dce396d7e640dd2308f108a495333d
Shiruvati <mrnattyboy05@gmail.com>
2025-02-15 23:26:44 +0530

    Update use-toast.ts

--- client/src/hooks/use-toast.ts
+++ client/src/hooks/use-toast.ts
@@ -63,7 +63,6 @@ export const reducer = (state: State, action: Action): State => {
       const { toastId } = action
 
       // ! Side effects ! - This could be extracted into a dismissToast() action,
-      // but I'll keep it here for simplicity
       if (toastId) {
         addToRemoveQueue(toastId)
       } else {
